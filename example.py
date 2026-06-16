f26190a855249a8572089af137e89c91f821520c
Nicolas Botelho <nicolasbotelho2005@gmail.com>
2026-06-15 21:38:49 -0300

    feat: using youtube transcript api

--- example.py
+++ example.py
@@ -2,7 +2,7 @@ from agno.agent import Agent
 from agno.tools.youtube import YouTubeTools
 # from agno.tools.visualization import VisualizationTools
 from agno.media import Video
-import os
+from youtube_transcript_api import YouTubeTranscriptApi
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -17,6 +17,7 @@ You are tasked with summarizing the video below focusing on its context and main
 - Answer with text only
 - Make your answer in topics, following the main points of the video
 - The first topic must be a introduction to the general context of the video and the last topic must a conclusion
+- If you can not use the Youtube Tool in any meaningful way, use the given transcript
 """
 
 hello_world = Agent(
@@ -27,9 +28,21 @@ hello_world = Agent(
   markdown=True,
   telemetry=True)
 
-url = "https://youtu.be/GmwaJnj6pfY?si=DN0KDmlW70cLlw6V"
+# url = "https://youtu.be/GmwaJnj6pfY?si=DN0KDmlW70cLlw6V"
+url = "https://youtu.be/zs_zpMnwNMs?si=m04Rhxf-OFniH12e"
+# url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUXbmV2ZXIgZ29ubmEgZ2l2ZSB5b3UgdXA%3D"
 
-result = hello_world.run(url)
+yta = YouTubeTranscriptApi()
+trasncript = ""
 
-print(result.content)
-print(result.metrics)
+try:
+  fetched = yta.fetch("zs_zpMnwNMs", languages=['pt', 'en', 'sp', 'fr'])
+  for snippet in fetched:
+    trasncript += snippet.text + "\n"
+except:
+  trasncript = "no transcript"
+finally:
+  result = hello_world.run('# Video URL\n' + url + f"\n\n# Transcript\n{trasncript}")
+
+  print(result.content)
+  print(result.metrics)
